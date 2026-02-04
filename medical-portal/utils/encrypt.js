32be248cce8b069b03971fd9590a43b3960183d0
Nihal Shetty <154510798+NihalShetty04@users.noreply.github.com>
2026-02-04 11:51:19 +0530

    Delete medical-portal/utils directory

--- medical-portal/utils/encrypt.js
+++
@@ -1,5 +0,0 @@
-exports.encrypt = (data) =>
-  Buffer.from(JSON.stringify(data)).toString("base64")
-
-exports.decrypt = (data) =>
-  JSON.parse(Buffer.from(data, "base64").toString())
--- medical-portal/utils/parser.js
+++
@@ -1,8 +0,0 @@
-const csv = require("csvtojson")
-const xml2js = require("xml2js")
-
-exports.parseData = async (text, type) => {
-  if (type === "json") return JSON.parse(text)
-  if (type === "csv") return await csv().fromString(text)
-  if (type === "xml") return await xml2js.parseStringPromise(text)
-}
